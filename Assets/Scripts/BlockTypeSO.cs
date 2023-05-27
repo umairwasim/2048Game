c1b5716b692550c3735e8cfb7deaea358e2b35bb
Umair <umairwasim001@gmail.com>
2023-05-27 23:28:10 +0500

    Create BlockTypeSO.cs

---
+++ Assets/Scripts/BlockTypeSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="", menuName ="Block Type Data")]
+public class BlockTypeSO : ScriptableObject
+{
+    public int value;
+    public Sprite sprite;
+}
